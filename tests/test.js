3c5bb3ca654ccdf206f7f8ea4139e607df54cfa8
Joshua Lanese <froastj@gmail.com>
2015-10-16 20:28:58 +0800

    Update

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ var request = require('request');
 var jar = request.jar();
 
 var opt2 = {
-    jar: jar,
+    //jar: jar,
     success: console.log,
     failure: function(err) {
         console.log('Fail: ' + err);
@@ -13,14 +13,42 @@ var opt2 = {
     }
 };
 
+var sopt = {
+    group: 147864,
+    roleset: 791521,
+    target: 21303105,
+    //jar: jar,
+    success: function() {
+        console.log('woot');
+    },
+    failure: function(err) {
+        console.log('Fail set: ' + err);
+    },
+    always: function() {
+        console.log('Done set');
+    }
+};
+
+var mopt = {
+  recipient: 21303049,
+  subject: 'Test',
+  body: Date.now().toString()
+};
+
 var options = {
-    username: 'username',
-    password: 'password',
-    jar: jar,
+    username: '',
+    password: '',
+    //jar: jar,
     success: function() {
         console.log('Logged in');
-        console.log(rjs.getSession(jar));
         rjs.getCurrentUser(opt2);
+        //rjs.getToken('http://www.roblox.com/messages/send',jar,console.log,console.log);
+
+        //rjs.message(21303049,'Test',Date.now().toString(),undefined,jar,console.log,console.log);
+        rjs.message(mopt);
+
+        //rjs.setRank(sopt);
+        //rjs.setRank(sopt.group,sopt.target,sopt.roleset,undefined,sopt.jar,sopt.success,sopt.failure,sopt.always);
     },
     failure: function(err) {
         console.log('Fail: ' + err);
@@ -31,5 +59,4 @@ var options = {
 };
 
 rjs.login(options);
-
 //rjs.login(options.username,options.password,options.jar,options.success,options.failure,options.always);
