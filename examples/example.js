5ab2bd8e0c14d04c02c0de9ef62a40a7402057e2
FroastJ <froastj@gmail.com>
2015-10-31 18:39:50 +0800

    Initial

---
+++ examples/example.js
@@ -0,0 +1,9 @@
+var rbx = require('roblox-js')
+var user = {
+  username: 'Shedletsky',
+  password: 'hunter2',
+  success: function() {
+    rbx.getCurrentUser(undefined,undefined,console.log)
+  }
+}
+rbx.login(user)
