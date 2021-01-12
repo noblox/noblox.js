f7106c78784584959ba34e06fb077115cf54648d
suufi <12759332+suufi@users.noreply.github.com>
2021-01-12 01:30:18 -0500

    Update Discord link

--- postinstall.js
+++ postinstall.js
@@ -12,8 +12,9 @@ ${chalk.underline('Thank you for installing noblox.js.')}
 
 ${chalk.bold('Documentation:')} https://noblox.js.org/ 
 ${chalk.bold('GitHub:')} https://github.com/suufi/noblox.js
-${chalk.bold('Support:')} https://discord.gg/FChzgQ8
+${chalk.bold('Support:')} https://discord.gg/R5GVSyTVGv
 
 ${chalk.bold.blue('noblox.js is maintained with the help of its users but sometimes Roblox silently updates its API endpoints breaking noblox.js out of the blue.')}
 ${chalk.green('We have created a request for Roblox to implement a change log to help keep the project\'s endpoints updated which you can find here: https://devforum.roblox.com/t/introduce-change-logs-to-roblox-api-endpoints/524783.')}
+${chalk.bgGreen('We also have our very own discord.js for support and informational purposes. To stay updated on new updates and bugs, you can join our Discord server with the following link: https://discord.gg/R5GVSyTVGv')}
 `)
