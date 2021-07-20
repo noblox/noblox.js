b14b3f86c3b210d98a95f3d27c9db3cac673fa86
suufi <12759332+suufi@users.noreply.github.com>
2021-07-20 18:46:06 -0400

    Deprecate the follow method & update test functions (#391)
    * Deprecate follow & remove from tests
    
    * Update test functions & add new ones

--- test/user.test.js
+++ test/user.test.js
@@ -1,4 +1,4 @@
-const { acceptFriendRequest, block, unblock, canManage, declineFriendRequest, follow, unfollow, getBlurb, getCollectibles, getFollowers, getFollowings, getFriendRequests, getFriends, getGroups, getIdFromUsername, getInventory, getInventoryById, getMessages, getOwnership, getPlayerBadges, getPlayerInfo, getPlayerThumbnail, getStatus, getUserSocialLinks, getUserTransactions, getUsernameFromId, removeFriend, sendFriendRequest, setCookie } = require('../lib')
+const { acceptFriendRequest, block, unblock, canManage, declineFriendRequest, unfollow, getBlurb, getCollectibles, getFollowers, getFollowings, getFriendRequests, getFriends, getGroups, getIdFromUsername, getInventory, getInventoryById, getMessages, getOwnership, getPlayerBadges, getPlayerInfo, getPlayerThumbnail, getPremium, getStatus, getUserSocialLinks, getUserTransactions, getUsernameFromId, getUAIDs, removeFriend, sendFriendRequest, setCookie } = require('../lib')
 
 beforeAll(() => {
   return new Promise(resolve => {
@@ -70,9 +70,6 @@ expect.extend({
 })
 
 describe('User Methods', () => {
-  it('follow() follows a user on Roblox', () => {
-    return follow(55549140)
-  })
 
   it('unfollow() unfollows a user on Roblox', () => {
     return unfollow(55549140)
@@ -330,6 +327,12 @@ describe('User Methods', () => {
     })
   })
 
+  it('getPremium() returns a player\'s premium state', () => {
+    return getPremium(55549140).then((res) => {
+      return expect(res).toEqual(expect.any(Boolean))
+    })
+  })
+
   it('getStatus() returns a player\'s status', () => {
     return getStatus(55549140).then((res) => {
       return expect(res).toEqual(expect.any(String))
@@ -342,6 +345,15 @@ describe('User Methods', () => {
     })
   })
 
+  it('getUAIDs() returns a user specific asset IDs given an ID', () => {
+    return getUAIDs(80231025, [1974901902, 4255053867, 2705893733, 1532395]).then((res) => {
+      return expect(res).toMatchObject({
+        uaids: expect.any(Array),
+        failedIds: expect.any(Array)
+      })
+    })
+  })
+  
   it('getUserSocialLinks() returns a player\'s promotion channel links', () => {
     return getUserSocialLinks(2416399685).then((res) => {
       return expect(res).toMatchObject({
