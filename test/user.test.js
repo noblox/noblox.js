936a2cf1367d37e62ee3798c2577231855a379ba
suufi <12759332+suufi@users.noreply.github.com>
2021-07-20 21:45:24 +0000

    Update test functions & add new ones

--- test/user.test.js
+++ test/user.test.js
@@ -1,4 +1,4 @@
-const { acceptFriendRequest, block, unblock, declineFriendRequest, follow, unfollow, getBlurb, getCollectibles, getFollowers, getFollowings, getFriendRequests, getFriends, getGroups, getIdFromUsername, getInventory, getInventoryById, getMessages, getOwnership, getPlayerBadges, getPlayerInfo, getPlayerThumbnail, getStatus, getUserTransactions, getUsernameFromId, removeFriend, sendFriendRequest, setCookie } = require('../lib')
+const { acceptFriendRequest, block, unblock, declineFriendRequest, unfollow, getBlurb, getCollectibles, getFollowers, getFollowings, getFriendRequests, getFriends, getGroups, getIdFromUsername, getInventory, getInventoryById, getMessages, getOwnership, getPlayerBadges, getPlayerInfo, getPlayerThumbnail, getPremium, getStatus, getUserTransactions, getUsernameFromId, getUAIDs, removeFriend, sendFriendRequest, setCookie } = require('../lib')
 
 beforeAll(() => {
   return new Promise(resolve => {
@@ -321,6 +321,12 @@ describe('User Methods', () => {
     })
   })
 
+  it('getPremium() returns a player\'s premium state', () => {
+    return getPremium(55549140).then((res) => {
+      return expect(res).toEqual(expect.any(Boolean))
+    })
+  })
+
   it('getStatus() returns a player\'s status', () => {
     return getStatus(55549140).then((res) => {
       return expect(res).toEqual(expect.any(String))
@@ -330,6 +336,15 @@ describe('User Methods', () => {
   it('getUsernameFromId() returns a player\'s username given an ID', () => {
     return getUsernameFromId(1).then((res) => {
       return expect(res).toEqual(expect.any(String))
+    })
+  })
+
+  it('getUAIDs() returns a user specific asset IDs given an ID', () => {
+    return getUAIDs(80231025, [1974901902, 4255053867, 2705893733, 1532395]).then((res) => {
+      return expect(res).toMatchObject({
+        uaids: expect.any(Array),
+        failedIds: expect.any(Array)
+      })
     })
   })
 
