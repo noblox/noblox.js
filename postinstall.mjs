ca19b1d69764748e0ddfba64e72b20567a8fdfe0
Josh <josh@muir.xyz>
2026-03-29 23:47:43 +0100

    Update postinstall.mjs

--- postinstall.mjs
+++ postinstall.mjs
@@ -13,8 +13,7 @@ ${chalk.underline('Thank you for installing noblox.js.')}
 
 ${chalk.bold('Documentation:')} https://noblox.js.org/ 
 ${chalk.bold('GitHub:')} https://github.com/noblox/noblox.js
-${chalk.bold('Support:')} https://discord.gg/R5GVSyTVGv
 
-${chalk.bold.blue('noblox.js is maintained with the help of its users but sometimes Roblox silently updates its API endpoints breaking noblox.js out of the blue.')}
-${chalk.bgGreen('We have our very own Discord for support and informational purposes. To stay updated on new updates and bugs, you can join our Discord server with the following link: https://discord.gg/R5GVSyTVGv')}
+${chalk.bold.red('noblox.js is no longer maintained. Do not use it for new work, and stop using it as soon as possible.')}
+${chalk.bgGreen('Discord server: https://discord.gg/R5GVSyTVGv')}
 `)
